a1fb7af238eaf9b14a92085bc783b45fdbc747f7
Agustin <agusg240428@gmail.com>
2026-02-16 18:04:57 -0300

    agrego imagenes

--- enviar.php
+++ enviar.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Reemplazá esta dirección por tu correo real
@@ -33,4 +34,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     echo "Acceso no permitido.";
 }
+=======
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Reemplazá esta dirección por tu correo real
+    $destino = "info@steffenargentina.com.ar";
+
+    $provincia = htmlspecialchars($_POST["provincia"]);
+    $rubro = htmlspecialchars($_POST["rubro"]);
+    $localidad = htmlspecialchars($_POST["localidad"]);
+    $nombre = htmlspecialchars($_POST["nombre"]);
+    $apellido = htmlspecialchars($_POST["apellido"]);
+    $telefono = htmlspecialchars($_POST["telefono"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $mensaje = htmlspecialchars($_POST["mensaje"]);
+
+    $asunto = "Nuevo mensaje desde el formulario web";
+    $contenido = "
+    Nombre: $nombre\n
+    Teléfono: $telefono\n
+    Email: $email\n
+    Provincia: $provincia\n
+    Rubro: $rubro\n
+    Localidad/Ciudad: $localidad\n
+    Mensaje:\n$mensaje
+    ";
+
+    $headers = "From: $email\r\nReply-To: $email";
+
+    if (mail($destino, "Nuevo mensaje", $contenido, $headers)) {
+        echo "¡Gracias! Tu mensaje ha sido enviado correctamente. Te responderemos a la brevedad.";
+    } else {
+        echo "Error al enviar el mensaje.";
+    }
+} else {
+    echo "Acceso no permitido.";
+}
+>>>>>>> c457847 (Agrego fotos)
 ?>
